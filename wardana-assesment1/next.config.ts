baf48a558da1feece3b4c1f9331705c9e7f95c1a
wardana <wardana.herna@tma.web.id>
2025-08-03 19:22:13 +0700

    modified:   wardana-assesment1/next.config.ts

--- wardana-assesment1/next.config.ts
+++ wardana-assesment1/next.config.ts
@@ -1,10 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/', // The incoming request path pattern
+        destination: '/home', // The path you want to redirect to
+        permanent: true, // Use 308 status code (permanent redirect)
+      },
+    ];
+  },
   sassOptions: {
     silenceDeprecations: ["legacy-js-api"],
   }
 };
-
-export default nextConfig;
